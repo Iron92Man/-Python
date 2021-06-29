4a3c786ee393b13f8eacbf296122b91e43e13bed
Iron92Man <maxim.zhelonkin@gmail.com>
2021-06-29 19:11:53 +0300

    task 1 for review

--- task_1.py
+++ task_1.py
@@ -8,4 +8,4 @@ var_two = 'Bye'
 
 user_name = input('What is your name?')
 user_age = int(input('How old are you?'))
-print(f"{var_one} {user_name}! You are already {user_age}! Long time no see...'")
+print(f"{var_one} {user_name}! You are already {user_age}! Long time no see...' {var_two}")
